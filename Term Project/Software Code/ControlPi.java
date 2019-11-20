f0b67ffedfb4d9b77533c510f7dd959c6d5a7c21
Natalie Chukwueke <nataliechukwueke@cmail.carleton.ca>
2019-11-20 02:56:24 -0500

    Update ControlPi.java

--- Term Project/Software Code/ControlPi.java
+++ Term Project/Software Code/ControlPi.java
@@ -10,7 +10,6 @@
  * 
  */
 package controlpi;
-
 /**
  *
  * @author Natalie
@@ -18,26 +17,31 @@ package controlpi;
 public class ControlPi {
     
     public void inputOptimalCondidionTemp(Float optTemp){
-        
+        String query = "INSERT INTO optimal_conditions(Optimal_Temperature); VALUES("+optTemp+")";
     }  
     
     public void inputOptimalCondidionHumidity(Float optHumidity){
-        
+        String query = "INSERT INTO optimal_conditions(Optimal_Humidity); VALUES("+optHumidity+")";
     } 
     
     public void inputOptimalCondidionWaterTime(Float waterTime){
-        
+        String query = "INSERT INTO optimal_conditions(Optimal_WaterTime); VALUES("+waterTime+")";
     } 
     
     public void inputOptimalConditionWaterInterval(Float timeStart, Float timeEnd){
-        
+        String query = "INSERT INTO optimal_conditions(Optimal_WaterStart,Optimal_WaterEnd); VALUES("+timeStart+","+timeEnd+")";
     }
     
+    public void inputOptimalConditonLightLevel(int lightLevel){
+        String query = "INSERT INTO optimal_conditions(Optimal_LightLevel); VALUES("+lightLevel+")";    
+    }
     public void Login(String username, String password){
         
     }
     
-    public void getCurrentStats(){
-        
+    public Object[] getCurrentStats(){
+        Object[] data = new Object[6];
+        data = GetData.getCurrentData();
+        return data;
     }
 }
