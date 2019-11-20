9a3edb671b27550366c5295fad08e92dc925c877
Natalie Chukwueke <nataliechukwueke@cmail.carleton.ca>
2019-11-20 01:07:39 -0500

    Create ControlPi.Java

---
+++ Term Project/Software Code/ControlPi.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * This class is used to create the user interface that allows the user to
+ * to request for data and access the database
+ * 
+ */
+package controlpi;
+
+/**
+ *
+ * @author Natalie
+ */
+public class ControlPi {
+    
+    public void inputOptimalCondidionTemp(Float optTemp){
+        
+    }  
+    
+    public void inputOptimalCondidionHumidity(Float optHumidity){
+        
+    } 
+    
+    public void inputOptimalCondidionWaterTime(Float waterTime){
+        
+    } 
+    
+    public void inputOptimalConditionWaterInterval(Float timeStart, Float timeEnd){
+        
+    }
+    
+    public void Login(String username, String password){
+        
+    }
+    
+    public void getCurrentStats(){
+        
+    }
+}
