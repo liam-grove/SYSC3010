d19f4af19df16fec0c521d576f8d5656033f8942
Natalie Chukwueke <nataliechukwueke@cmail.carleton.ca>
2019-11-20 08:04:11 -0500

    Update ControlPi.java

--- Term Project/Software Code/ControlPi.java
+++ Term Project/Software Code/ControlPi.java
@@ -15,33 +15,64 @@ package controlpi;
  * @author Natalie
  */
 public class ControlPi {
-    
+   /**
+     * This method inputs the optimal temperature into the database
+     * @param optTemp 
+     */
     public void inputOptimalCondidionTemp(Float optTemp){
         String query = "INSERT INTO optimal_conditions(Optimal_Temperature); VALUES("+optTemp+")";
+        byte[] request = GetData.sendMessageToServer(query);
     }  
     
+    /**
+     * This method inputs the optimal humidity into the database
+     * @param optHumidity 
+     */
     public void inputOptimalCondidionHumidity(Float optHumidity){
         String query = "INSERT INTO optimal_conditions(Optimal_Humidity); VALUES("+optHumidity+")";
+        byte[] request = GetData.sendMessageToServer(query);
     } 
     
+    /**
+     * This method inputs the optimal amount of time needed to water the plant into the database
+     * @param waterTime 
+     */
     public void inputOptimalCondidionWaterTime(Float waterTime){
         String query = "INSERT INTO optimal_conditions(Optimal_WaterTime); VALUES("+waterTime+")";
+        byte[] request = GetData.sendMessageToServer(query);
     } 
     
+    /**
+     * This method inputs the optimal time interval before the next watering of the plant occurs into the database
+     * @param timeStart
+     * @param timeEnd 
+     */
     public void inputOptimalConditionWaterInterval(Float timeStart, Float timeEnd){
         String query = "INSERT INTO optimal_conditions(Optimal_WaterStart,Optimal_WaterEnd); VALUES("+timeStart+","+timeEnd+")";
+        byte[] request = GetData.sendMessageToServer(query); //This will be sent through the client to the server
     }
     
+    /**
+     * This method inputs the optimal light level into the database
+     * @param lightLevel 
+     */
     public void inputOptimalConditonLightLevel(int lightLevel){
         String query = "INSERT INTO optimal_conditions(Optimal_LightLevel); VALUES("+lightLevel+")";    
+        byte[] request = GetData.sendMessageToServer(query);
     }
+    
     public void Login(String username, String password){
         
     }
     
-    public Object[] getCurrentStats(){
+    /**
+     * This method gets the last inputted entry that was put into the database
+     * in the table collected_data
+     * @return 
+     */
+    public String getCurrentStats(){
         Object[] data = new Object[6];
         data = GetData.getCurrentData();
-        return data;
+        return Arrays.deepToString(data);
     }
 }
