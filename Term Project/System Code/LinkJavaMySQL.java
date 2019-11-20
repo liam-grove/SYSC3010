22c9e7b3467ace1cb73f99cab005e6389da0cc10
liam-grove <49702541+liam-grove@users.noreply.github.com>
2019-11-20 08:48:37 -0500

    Update LinkJavaMySQL.java

--- Term Project/System Code/LinkJavaMySQL.java
+++ Term Project/System Code/LinkJavaMySQL.java
@@ -1,14 +1,76 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gem;
 
-/**
- *
- * @author sakay
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 public class LinkJavaMySQL {
+    
+    //JDBC URL, username and password of MySQL server
+    private static final String url = "jdbc:mysql://134.117.249.1:3306/greenhouse";
+    private static final String user = "natalie";
+    private static final String password = "password";
+    
+    //JDBC variables for opening and managing connection
+    private static Connection con;
+    private static Statement stmt;
+    private static ResultSet rs;
+    
+    public static void main(String args[]){
+        
+        
+    }
+    
+    public ResultSet selectQuery(String query) {
+        
+        try {
+            //open database connection
+            con = DriverManager.getConnection(url, user, password);
+            
+            //getting Statement object to execute query
+            stmt = con.createStatement();
+            
+            //executing SELECT query
+            rs = stmt.executeQuery(query);
+            
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        } finally {
+            try { con.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { rs.close(); } catch(SQLException se) { /*can't do anything */ }
+        }
+        
+        return rs;
+    }
+    
+    public boolean insertQuery(String query) {
+        
+        boolean result = true;
+        
+        try {
+            //open database connection
+            con = DriverManager.getConnection(url, user, password);
+            
+            //getting Statement object to execute query
+            stmt = con.createStatement();
+            
+            //executing INSERT query
+            stmt.executeUpdate(query);
+            
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+            result = false;
+            
+        } finally {
+            try { con.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
+        }
+        
+        return result;
+    }
     
 }
