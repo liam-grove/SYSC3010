04425f87ba40a755319d2c9a6e3b95db030947fb
liam-grove <49702541+liam-grove@users.noreply.github.com>
2019-12-02 18:48:39 -0500

    Update pi-arduino-dht.py

--- Term Project/System Code/pi-arduino-dht.py
+++ Term Project/System Code/pi-arduino-dht.py
@@ -1,31 +1,35 @@
 #rpi-arduino-dht11
 import serial, socket, string, sys, time, pymysql
 
-serverIP = "172.17.61.82"
-serverPort = 9876
+serverIP = "172.20.10.3"
+serverPort = 56789
 
 
-try:
-    conn = pymysql.connect(host = '172.17.55.14',
-                           user='natalie',
-                           passwd='password',
-                           db='greenhouse',
-                           charset='utf8mb4',
-                           cursorclass=pymysql.cursors.DictCursor)
+#try:
+ #   conn = pymysql.connect(host = '172.17.55.14',
+  #                         user='natalie',
+   #                        passwd='password',
+    #                       db='greenhouse',
+     #                      charset='utf8mb4',
+      #                     cursorclass=pymysql.cursors.DictCursor)
     # Create a Cursor object to execute queries.
-    cur = conn.cursor()
+  #  cur = conn.cursor()
 
-except Exception as e:
-    print("Exeception occured:{}".format(e))
+#except Exception as e:
+ #   print("Exeception occured:{}".format(e))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientport = 56789
 serverAddress = (serverIP, serverPort)
-arduino = serial.Serial('/dev/ttyUSB0', 9600)
-sock.bind(serverAddress)
+arduino = serial.Serial('/dev/ttyACM0', 9600)
+#sock.bind((serverIP, serverPort))
 
 wateringActive = 0 #time between turning water on/off
 wateringInterval = 0 #time between watering cycles
+
+bootMessage = "dpi:ser:BOOT:test"
+sock.sendto(bootMessage.encode('utf-8'), (serverIP, serverPort))
+print("Boot message sent")
 
 while True:
         if arduino.in_waiting > 0:
@@ -36,16 +40,16 @@ while True:
             temperature = splitMessage[1]
             humidity = splitMessage[2].strip('>')
             
-            queryTemplate = "INSERT INTO collected_data(Username, Date, Time, Temperature, Humidity, LightLevel) VALUES({},{},{},{},{},{})"
-            query = queryTemplate.format(username, date, time, temperature, humidity, light)
+            #queryTemplate = "INSERT INTO collected_data(Username, Date, Time, Temperature, Humidity, LightLevel) VALUES({},{},{},{},{},{})"
+            #query = queryTemplate.format(username, date, time, temperature, humidity, light)
             
-            cur.execute(query)
+            #cur.execute(query)
             
             dataMessageTemplate = "<{},{},{}>"
             dataMessage = dataMessageTemplate.format(light, temperature, humidity)
             
             controlPiMessage = "dpi:cpi:data:" + dataMessage
-            #print(controlPiMessage)
+            print(controlPiMessage)
             sock.sendto(controlPiMessage.encode('utf-8'), serverAddress)
         
         #buf, address = sock.recvfrom(port)
