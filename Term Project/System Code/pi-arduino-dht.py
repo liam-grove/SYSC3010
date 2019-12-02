72e3df9e3eeeabe37419f14c4cf2f439c7fbc499
liam-grove <49702541+liam-grove@users.noreply.github.com>
2019-12-02 13:49:02 -0500

    Update pi-arduino-dht.py

--- Term Project/System Code/pi-arduino-dht.py
+++ Term Project/System Code/pi-arduino-dht.py
@@ -1,25 +1,48 @@
 #rpi-arduino-dht11
 #Raspberry Pi reads temperature and humidity sensor data from Arduino
 import serial, socket, string, sys, time
+#, MySQLdb
 
-serverHost = sys.argv[1]
-serverPort = sys.argv[2]
+serverIP = 172.17.61.82
+serverPort = 9876
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-port = int(serverPort)
+clientport = 56789
 serverAddress = (serverHost, port)
-ser = serial.Serial('/dev/ttyUSB0', 9600)
+arduino = serial.Serial('/dev/ttyUSB0', 9600)
+sock.bind(serverAddress)
+
+wateringActive = 0 #time between turning water on/off
+wateringInterval = 0 #time between watering cycles
 
 while True:
-        if ser.in_waiting > 0:
-            rawMessage = ser.readline()
+        if arduino.in_waiting > 0:
+            rawMessage = arduino.readline()
             decodedMessage = rawMessage.decode('utf-8').strip('\r\n')
             splitMessage = decodedMessage.split(',')
             light = splitMessage[0].strip('<')
             temperature = splitMessage[1]
             humidity = splitMessage[2].strip('>')
             
-            sock.sendto(decodedMessage.encode('utf-8'), serverAddress)
+            dataMessageTemplate = "<{},{},{}>"
+            dataMessage = dataMessageTemplate.format(light, temperature, humidity)
+            
+            controlPiMessage = "dpi:cpi:data:" + dataMessage
+            #print(controlPiMessage)
+            sock.sendto(controlPiMessage.encode('utf-8'), serverAddress)
+            
+        buf, address = sock.recvfrom(port)
+        
+        #receive incoming messages
+        buf
+        
+        #check incoming message validity
+        
+        #send command to arduino
+        
+        #
+        
+            
             
 sock.shutdown(1)
             
