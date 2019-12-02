94222e48dcef5a77123453116ce91dc6b57fe8dc
liam-grove <49702541+liam-grove@users.noreply.github.com>
2019-12-02 15:15:29 -0500

    Update pi-arduino-dht.py

--- Term Project/System Code/pi-arduino-dht.py
+++ Term Project/System Code/pi-arduino-dht.py
@@ -1,14 +1,26 @@
 #rpi-arduino-dht11
-#Raspberry Pi reads temperature and humidity sensor data from Arduino
-import serial, socket, string, sys, time
-#, MySQLdb
+import serial, socket, string, sys, time, pymysql
 
-serverIP = 172.17.61.82
+serverIP = "172.17.61.82"
 serverPort = 9876
+
+
+try:
+    conn = pymysql.connect(host = '172.17.55.14',
+                           user='natalie',
+                           passwd='password',
+                           db='greenhouse',
+                           charset='utf8mb4',
+                           cursorclass=pymysql.cursors.DictCursor)
+    # Create a Cursor object to execute queries.
+    cur = conn.cursor()
+
+except Exception as e:
+    print("Exeception occured:{}".format(e))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientport = 56789
-serverAddress = (serverIP, port)
+serverAddress = (serverIP, serverPort)
 arduino = serial.Serial('/dev/ttyUSB0', 9600)
 sock.bind(serverAddress)
 
@@ -24,25 +36,26 @@ while True:
             temperature = splitMessage[1]
             humidity = splitMessage[2].strip('>')
             
+            queryTemplate = "INSERT INTO collected_data(Username, Date, Time, Temperature, Humidity, LightLevel) VALUES({},{},{},{},{},{})"
+            query = queryTemplate.format(username, date, time, temperature, humidity, light)
+            
+            cur.execute(query)
+            
             dataMessageTemplate = "<{},{},{}>"
             dataMessage = dataMessageTemplate.format(light, temperature, humidity)
             
             controlPiMessage = "dpi:cpi:data:" + dataMessage
             #print(controlPiMessage)
             sock.sendto(controlPiMessage.encode('utf-8'), serverAddress)
-            
-        buf, address = sock.recvfrom(port)
+        
+        #buf, address = sock.recvfrom(port)
         
         #receive incoming messages
-        buf
         
         #check incoming message validity
         
         #send command to arduino
         
-        #
-        
             
             
 sock.shutdown(1)
-            
