45952cf4fbdf5c31e92b081cb2497b57eae90d5f
NA25198 <nirda_ali@hotmail.com>
2019-12-02 21:06:29 -0500

    Update UDPClient.java

--- Term Project/Software Code/UDPClient.java
+++ Term Project/Software Code/UDPClient.java
@@ -1,5 +1,6 @@
 package gem;
 
+import java.io.IOException;
 import java.net.*;
 import java.util.ArrayList;
 
@@ -7,25 +8,26 @@ import java.util.ArrayList;
  *
  * @author Nirda
  */
-public class UDPClient 
+public class UDPClient implements Runnable
 {
-    //client ip address 
+     //client ip address 
     private InetAddress ip;
     //server port 
-    private static final int ServerPort = 9876; 
-    //server ip address 
-    
+    private static final int ServerPort = 56789; 
 
-     public static void main(String args[]) throws Exception
-    {
-        InetAddress ServerAddress = InetAddress.getByName("172.17.61.82");
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws UnknownHostException, SocketException, IOException {
+        /*InetAddress ServerAddress = InetAddress.getByName("172.20.10.3");
         
         //DatagramSocket controlPiSocket;
         
-        DatagramSocket clientSocket = new DatagramSocket(ServerPort);
+        DatagramSocket clientSocket = new DatagramSocket();
         byte[] receiveData = new byte[56];
         byte[] sendData = new byte[50];
-        byte[] ack = {'A', 'C', 'K'};
+        //byte[] ack = {'A', 'C', 'K'};
+        String ack = "ACK";
         
         //Send Boot up message to server 
         byte[] IM = new byte[50];
@@ -38,19 +40,25 @@ public class UDPClient
         {
             //Create and receive incoming packet
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            System.out.println("here");
             clientSocket.receive(receivePacket);
+            
             String incomingMessage = new String(receivePacket.getData());
+            System.out.println("incoming message: " + incomingMessage);
             
             String[] splitMessage = incomingMessage.split(":");
+            //System.out.println(splitMessage);
             String source = splitMessage[0];
             String destination = splitMessage[1];
             String code = splitMessage[2];
             String data = splitMessage[3];
             
+            System.out.println("data: " + data);
+            //System.out.println(ack.toString());
             //check if incoming packet is acknowledgement
             if (code.equals(ack.toString()))
             {
-                     
+                   System.out.println("yay");  
                     //confirmation of acknowledgement
                      
             }
@@ -58,9 +66,9 @@ public class UDPClient
             else
             {
                 String[] dataValues = data.split(",");
-                String light = dataValues[0];
+                String light = dataValues[0].replace("<", "");
                 String temp = dataValues[1];
-                String humidity = dataValues[2];
+                String humidity = dataValues[2].replace(">","");
                 
                 String lightStr = light.substring(1);
                 String humidtyStr = humidity.substring(0,humidity.length()-1);
@@ -69,10 +77,93 @@ public class UDPClient
                 float h = Float.valueOf(humidtyStr);
                 int l = Integer.valueOf(light);
                 
-                System.out.println("temp: " + t + " humidity: " + h + " light: " + l + " .");
+                System.out.println("temp: " + t + " humidity: " + h + " light: " + l );
+            
+            }
+        }*/
+    }
+
+    @Override
+    public void run() {
+        
+        
+        
+        try{
+            
+        
+        
+        InetAddress ServerAddress = InetAddress.getByName("172.20.10.3");
+        
+        //DatagramSocket controlPiSocket;
+        
+        DatagramSocket clientSocket = new DatagramSocket();
+        byte[] receiveData = new byte[56];
+        byte[] sendData = new byte[50];
+        //byte[] ack = {'A', 'C', 'K'};
+        String ack = "ACK";
+        
+        //Send Boot up message to server 
+        byte[] IM = new byte[50];
+        String bootMessage = "cpi:ser:BOOT:test";
+        IM = bootMessage.getBytes();
+        DatagramPacket initalMessage = new DatagramPacket(IM,IM.length,ServerAddress,ServerPort);
+        clientSocket.send(initalMessage);
+        
+        while(true)
+        {
+            //Create and receive incoming packet
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            System.out.println("here");
+            clientSocket.receive(receivePacket);
+            
+            String incomingMessage = new String(receivePacket.getData());
+            System.out.println("incoming message: " + incomingMessage);
+            
+            String[] splitMessage = incomingMessage.split(":");
+            //System.out.println(splitMessage);
+            String source = splitMessage[0];
+            String destination = splitMessage[1];
+            String code = splitMessage[2];
+            String data = splitMessage[3];
+            
+            System.out.println("data: " + data);
+            //System.out.println(ack.toString());
+            //check if incoming packet is acknowledgement
+            if (code.equals(ack.toString()))
+            {
+                   System.out.println("yay");  
+                    //confirmation of acknowledgement
+                     
+            }
+            //otherwise it is going to be a automatic update for current stats 
+            else
+            {
+                
+
+                String[] dataValues = data.split(",");
+                String light = dataValues[0].replace("<", "");
+                String temp = dataValues[1];
+                String humidity = dataValues[2].replace(">","");
+                
+                String lightStr = light.substring(1);
+                String humidtyStr = humidity.substring(0,humidity.length()-1);
+                
+                float t = Float.valueOf(temp);
+                float h = Float.valueOf(humidtyStr);
+                int l = Integer.valueOf(light);
+                
+                MainWindow m = new MainWindow(t,h,l);
+                m.updateLabels(t, h, l);
+            
+                System.out.println("temp: " + t + " humidity: " + h + " light: " + l );
             
             }
         }
+    }catch(Exception e)
+     {
+         System.out.println(e);
+     }
     }
     
+     
 }  
