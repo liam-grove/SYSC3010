fac3d2731fe003e9466273e8be8a75e35074858b
liam-grove <49702541+liam-grove@users.noreply.github.com>
2019-12-02 20:36:28 -0500

    Update pi-arduino-dht.py

--- Term Project/System Code/pi-arduino-dht.py
+++ Term Project/System Code/pi-arduino-dht.py
@@ -1,5 +1,7 @@
 #rpi-arduino-dht11
-import serial, socket, string, sys, time, pymysql
+import serial, socket, string, sys, time #, pymysql
+from timeloop import Timeloop
+from datetime import timedelta
 
 serverIP = "172.20.10.3"
 serverPort = 56789
@@ -21,45 +23,65 @@ serverPort = 56789
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientport = 56789
 serverAddress = (serverIP, serverPort)
-arduino = serial.Serial('/dev/ttyACM0', 9600)
+arduino = serial.Serial('/dev/ttyACM1', 9600)
 #sock.bind((serverIP, serverPort))
 
-wateringActive = 0 #time between turning water on/off
-wateringInterval = 0 #time between watering cycles
+wateringActive = 5 #time between turning water on/off
+wateringInterval = 8 #time between watering cycles
 
 bootMessage = "dpi:ser:BOOT:test"
 sock.sendto(bootMessage.encode('utf-8'), (serverIP, serverPort))
 print("Boot message sent")
 
-while True:
-        if arduino.in_waiting > 0:
-            rawMessage = arduino.readline()
-            decodedMessage = rawMessage.decode('utf-8').strip('\r\n')
-            splitMessage = decodedMessage.split(',')
-            light = splitMessage[0].strip('<')
-            temperature = splitMessage[1]
-            humidity = splitMessage[2].strip('>')
-            
-            #queryTemplate = "INSERT INTO collected_data(Username, Date, Time, Temperature, Humidity, LightLevel) VALUES({},{},{},{},{},{})"
-            #query = queryTemplate.format(username, date, time, temperature, humidity, light)
-            
-            #cur.execute(query)
-            
-            dataMessageTemplate = "<{},{},{}>"
-            dataMessage = dataMessageTemplate.format(light, temperature, humidity)
-            
-            controlPiMessage = "dpi:cpi:data:" + dataMessage
-            print(controlPiMessage)
-            sock.sendto(controlPiMessage.encode('utf-8'), serverAddress)
+tl = Timeloop()
+
+@tl.job(interval=timedelta(seconds=3))
+def transmit_data():
+    
+    if arduino.in_waiting > 0:
+        rawMessage = arduino.readline()
+        decodedMessage = rawMessage.decode('utf-8').strip('\r\n')
+        splitMessage = decodedMessage.split(',')
+        light = splitMessage[0].strip('<')
+        temperature = splitMessage[1]
+        humidity = splitMessage[2].strip('>')
+                
+         #queryTemplate = "INSERT INTO collected_data(Username, Date, Time, Temperature, Humidity, LightLevel) VALUES({},{},{},{},{},{})"
+         #query = queryTemplate.format(username, date, time, temperature, humidity, light)
+                
+        #cur.execute(query)
+         
+        dataMessageTemplate = "<{},{},{}>"
+        dataMessage = dataMessageTemplate.format(light, temperature, humidity)
+                
+        controlPiMessage = "dpi:cpi:data:" + dataMessage
+        print(controlPiMessage)
+        sock.sendto(controlPiMessage.encode('utf-8'), serverAddress)
         
-        #buf, address = sock.recvfrom(port)
+@tl.job(interval=timedelta(seconds=10))
+def receive_data():
+    
+    data, addr = sock.recvfrom(1024)
+    print("Data received:")
+    print(data)
+    
+    splitData = data.split(':')
+    code = splitData[2]
+    if code == WATER1:
+        wateringInterval = splitData[3]
+    if code == WATER2:
+        wateringActive = splitData[3]
         
-        #receive incoming messages
-        
-        #check incoming message validity
-        
-        #send command to arduino
-        
-            
+@tl.job(interval=timedelta(seconds=wateringInterval))
+def activate_water():
+    
+    print("Activating motor")
+    arduino.write(1)
+    print(wateringActive)
+    
+if __name__ == "__main__":
+    tl.start(block=True)
+    
+                    
             
 sock.shutdown(1)
