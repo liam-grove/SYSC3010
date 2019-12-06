83302820468a68c646571811a7f3dd2883412e0d
NA25198 <nirda_ali@hotmail.com>
2019-12-06 18:17:29 -0500

    Update Report.java

--- Term Project/Software Code/Report.java
+++ Term Project/Software Code/Report.java
@@ -5,6 +5,13 @@
  */
 package gem;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Nirda
@@ -14,8 +21,9 @@ public class Report extends javax.swing.JFrame {
     /**
      * Creates new form Report
      */
-    public Report() {
+    public Report() throws SQLException, ClassNotFoundException {
         initComponents();
+        
     }
 
     /**
@@ -27,21 +35,93 @@ public class Report extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
+        PanelA = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        myTable = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
+        changeButton = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        PanelA.setBackground(new java.awt.Color(36, 47, 65));
+
+        myTable.setBackground(new java.awt.Color(36, 47, 65));
+        myTable.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(204, 204, 204)));
+        myTable.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
+        myTable.setForeground(new java.awt.Color(204, 204, 204));
+        myTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+
+            }
+        ));
+        jScrollPane1.setViewportView(myTable);
+
+        jLabel1.setFont(new java.awt.Font("Century Gothic", 0, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(204, 204, 204));
+        jLabel1.setText("10 Most Recent Updates");
+
+        changeButton.setText("Display");
+        changeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                changeButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout PanelALayout = new javax.swing.GroupLayout(PanelA);
+        PanelA.setLayout(PanelALayout);
+        PanelALayout.setHorizontalGroup(
+            PanelALayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(PanelALayout.createSequentialGroup()
+                .addGroup(PanelALayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(PanelALayout.createSequentialGroup()
+                        .addGap(40, 40, 40)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 418, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(PanelALayout.createSequentialGroup()
+                        .addGap(142, 142, 142)
+                        .addComponent(jLabel1))
+                    .addGroup(PanelALayout.createSequentialGroup()
+                        .addGap(207, 207, 207)
+                        .addComponent(changeButton)))
+                .addContainerGap(61, Short.MAX_VALUE))
+        );
+        PanelALayout.setVerticalGroup(
+            PanelALayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, PanelALayout.createSequentialGroup()
+                .addGap(62, 62, 62)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(33, 33, 33)
+                .addComponent(changeButton)
+                .addContainerGap(90, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 519, Short.MAX_VALUE)
+            .addComponent(PanelA, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 387, Short.MAX_VALUE)
+            .addComponent(PanelA, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>                        
+
+    private void changeButtonActionPerformed(java.awt.event.ActionEvent evt) {                                             
+        try {
+            makeTable();
+        } catch (SQLException ex) {
+            Logger.getLogger(Report.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Report.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }                                            
 
     /**
      * @param args the command line arguments
@@ -73,11 +153,46 @@ public class Report extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Report().setVisible(true);
+        
+                try {
+                    new Report().setVisible(true);
+                } catch (SQLException ex) {
+                    Logger.getLogger(Report.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(Report.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
             }
         });
     }
+    
+    public void makeTable() throws SQLException, ClassNotFoundException
+    {
+        String user = Login.getUsername();
+        String[] column = {"Date Time", "Temperature", "Humidity","Light Level"};
+        Object[][] data = ControlPi.getData();
+        
+        String[][] dataStr = new String[10][4]; 
+        System.out.println(data);
+        
+        for(int row=0; row<=9; row++)
+            {
+                for(int col=0; col <= 3; col++)
+                {
+                    dataStr[row][col] = data[row][col].toString();
+                   
+                }
+            }
+        DefaultTableModel model = (DefaultTableModel) myTable.getModel();
+        model.setDataVector(dataStr,column);
+
+    }
 
     // Variables declaration - do not modify                     
+    private javax.swing.JPanel PanelA;
+    private javax.swing.JButton changeButton;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable myTable;
     // End of variables declaration                   
 }
